b34474294fb86baadf96859d29642b28d746155e
Guilherme Samuel <guisamuel800@gmail.com>
2023-02-05 13:51:13 -0300

    chore: config firebase in flutter (android & ios)

---
+++ lib/menu.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class Menu extends StatefulWidget {
+  const Menu({Key? key}) : super(key: key);
+
+  @override
+  State<Menu> createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'TAUBATEXAS ROBOTICS',
+            style: TextStyle(
+              fontFamily: 'JackFrost',
+              fontSize: 20
+            ),
+          ),
+          leading: IconButton(
+            icon: Icon(Icons.list),
+            onPressed: null
+          ),
+          centerTitle: true,
+          elevation: 0,
+          foregroundColor: Colors.white,
+          bottom: TabBar(
+            labelStyle: TextStyle(
+              fontFamily: 'Roboto',
+              fontSize: 20,
+              fontWeight: FontWeight.bold
+            ),
+            tabs: <Tab>[
+              Tab(
+                icon: Icon(Icons.new_releases),
+                text: 'Alunos'
+              ),
+              Tab(
+                icon: Icon(Icons.extension),
+                text: 'Mentores'
+              ),
+            ]
+          )
+        ),
+        body: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            alignment: Alignment.center,
+            child: Column(
+              children: <Widget>[
+                
+              ]
+            )
+          )
+        )
+      )
+    );
+  }
+}
